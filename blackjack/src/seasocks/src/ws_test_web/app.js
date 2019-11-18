2dd1bc86975bd955bfaa5b7b2c9aa9a38dbfc0c4
mattwalo32 <mattwalowski@gmail.com>
2019-11-18 10:43:40 -0600

    Add frontend web pages

---
+++ blackjack/src/seasocks/src/ws_test_web/app.js
@@ -0,0 +1,32 @@
+var ws;
+
+$(function() {
+    ws = new WebSocket('ws://' + document.location.host + '/ws');
+    ws.onopen = function() {
+        console.log('onopen');
+        ws.send("SETTYPE:0"); // Type 0 is web client
+    };
+
+    ws.onclose = function() {
+        $('#message').text('Lost connection.');
+        console.log('onclose');
+    };
+
+    ws.onmessage = function(message) {
+        console.log("got '" + message.data + "'");
+        $("body").append("<p>" + message.data + "</p>");
+    };
+
+    ws.onerror = function(error) {
+        console.log('onerror ' + error);
+        console.log(error);
+    };
+
+    $('#sendNameButton').click(function() {
+    	ws.send("SETNAME:" + $('#name').val());
+    });
+
+    $('#sendCommand').click(function() {
+    	ws.send($('#command').val());
+    });
+});
