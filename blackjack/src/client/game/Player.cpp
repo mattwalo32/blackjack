045fd42ba034dd47685f9ff298e26aa9e51b040c
mattwalo32 <mattwalowski@gmail.com>
2019-11-28 20:56:20 -0600

    Create player object

--- blackjack/src/client/game/Player.cpp
+++ blackjack/src/client/game/Player.cpp
@@ -1,1 +1,43 @@
 #include "Player.h"
+
+Player::Player(int startingBalance) {
+	balance = startingBalance;
+	busted = false;
+}
+
+void Player::dealCard(Card card) {
+	currentHand.push_back(card);
+	
+	//TODO: Check if bust
+
+}
+
+void Player::addToBalance(int amount) {
+	balance += amount;
+}
+
+void Player::removeFromBalance(int amount) {
+	balance -= amount;
+}
+
+void Player::clearHand() {
+	currentHand.clear();
+	currentBet = 0;
+	busted = false;
+}
+
+void Player::setCurrentBet(int betAmount) {
+	currentBet = betAmount;
+}
+
+int Player::getBalance() {
+	return balance;
+}
+
+int Player::getCurrentBet() {
+	return currentBet;
+}
+
+bool Player::isBust() {
+	return busted;
+}
--- blackjack/src/client/game/Player.h
+++ blackjack/src/client/game/Player.h
@@ -1,6 +1,28 @@
 #pragma once
 
+#include <string>
+#include <vector>
+
+#include "Card.h"
+
 class Player {
+public:
+	Player(int startingBalance);
+	void dealCard(Card card);
+	void addToBalance(int amount);
+	void removeFromBalance(int amount);
+	void clearHand();
+	
+	void setCurrentBet(int betAmount);
+	int getCurrentBet();
+	int getBalance();
+	bool isBust();
 
+private:
+	int balance;
+	int currentBet;
+	bool busted;
 
+	std::string name;
+	std::vector<Card> currentHand;
 };
